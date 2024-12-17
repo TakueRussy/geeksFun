c5566befc8ff8206ec25507e04c197c10b91e9f1
TakueRussy <takudzwarusenza96@gmail.com>
2024-12-17 20:16:21 +0200

    Sum of array elements

---
+++ Sum on Array elements.py
@@ -0,0 +1,8 @@
+#You are given an integer array arr[]. The task is to find the sum of it.
+
+def _sum(arr):
+    # code here
+    total=0
+    for element in arr:
+        total+=element
+    return total
